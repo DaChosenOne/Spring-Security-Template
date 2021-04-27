cdad1174a529b09cf5fbc0b89a6fb2cd520bfcaf
Jesus Samano <doomlord97@gmail.com>
2021-04-27 11:05:41 -0500

    Estructura basica del proyecto

---
+++ src/main/java/com/samano/security/template/SpiringSecurityTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.samano.security.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiringSecurityTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiringSecurityTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samano/security/template/SpiringSecurityTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samano.security.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiringSecurityTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
